460bc456c0b2945ab87aa37949fc23060b7e215f
Lassaad Aissa Berraiess <33327085+Lassaadaissa@users.noreply.github.com>
2021-06-25 00:31:48 +0200

    Update BackgroundColor.java

--- app/src/main/java/com/example/lassaad/quizbee/BackgroundColor.java
+++ app/src/main/java/com/example/lassaad/quizbee/BackgroundColor.java
@@ -1,24 +1,22 @@
 package com.example.lassaad.quizbee;
 import android.graphics.Color;
-/**
- * Created by Sushant on 23-11-2016.
- */
+
 import java.util.Random;
 public class BackgroundColor {
     private String[] mColors = {
-            "#39add1", // light blue
-            "#3079ab", // dark blue
-            "#c25975", // mauve
-            "#e15258", // red
-            "#f9845b", // orange
-            "#838cc7", // lavender
-            "#7d669e", // purple
-            "#53bbb4", // aqua
-            "#51b46d", // green
-            "#e0ab18", // mustard
-            "#637a91", // dark gray
-            "#f092b0", // pink
-            "#b7c0c7" // light gray}
+            "#39add1", 
+            "#3079ab", 
+            "#c25975", 
+            "#e15258", 
+            "#f9845b", 
+            "#838cc7", 
+            "#7d669e", 
+            "#53bbb4", 
+            "#51b46d", 
+            "#e0ab18", 
+            "#637a91", 
+            "#f092b0", 
+            "#b7c0c7" 
     };
 public int getColor(){
     //Randomly generate a color
